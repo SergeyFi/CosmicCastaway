72a52954228b96caa1a1be07c5813444e61a08f7
Sergey Firsov <46263847+SergeyFi@users.noreply.github.com>
2021-10-28 21:16:16 +0300

    Initial commit

---
+++ Source/CosmicCastaway/CosmicCastawayGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CosmicCastawayGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COSMICCASTAWAY_API ACosmicCastawayGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
