383a39ceb73591c5bd98f2a165b4bc40d5db1eaf
Sergey Firsov <46263847+SergeyFi@users.noreply.github.com>
2021-11-03 14:58:10 +0300

    Feature: add rocket engine

---
+++ Source/CosmicCastaway/Components/RocketEngine.cpp
@@ -0,0 +1,112 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/RocketEngine.h"
+
+// Sets default values for this component's properties
+URocketEngine::URocketEngine()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+// Called when the game starts
+void URocketEngine::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetOwnerRoot();
+
+	BindToInput();
+	
+}
+
+void URocketEngine::GetOwnerRoot()
+{
+	OwnerRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+
+	if (!OwnerRoot)
+	{
+		SetComponentTickEnabled(false);
+	}
+}
+
+void URocketEngine::AddEngineForce(float DeltaTime)
+{
+	auto Force =
+		GetOwner()->GetActorForwardVector() * DeltaTime * Coefficient * Thrust * InputComponent->GetAxisValue("EngineForward");
+
+	OwnerRoot->AddForce(Force);
+}
+
+void URocketEngine::BindToInput()
+{
+	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+	InputComponent->BindAxis("EngineForward");
+
+	InputComponent->BindAction("FullStop", EInputEvent::IE_Pressed, this, &URocketEngine::FullStopTrue);
+	InputComponent->BindAction("FullStop", EInputEvent::IE_Released, this, &URocketEngine::FullStopFalse);
+}
+
+void URocketEngine::FullStopTrue()
+{
+	bFullStop = true;
+}
+
+void URocketEngine::FullStopFalse()
+{
+	bFullStop = false;
+}
+
+
+// Called every frame
+void URocketEngine::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	AddEngineForce(DeltaTime);
+
+	FullStop(DeltaTime);
+}
+
+void URocketEngine::StartEngine()
+{
+	SetComponentTickEnabled(true);
+}
+
+void URocketEngine::StopEngine()
+{
+	SetComponentTickEnabled(false);
+}
+
+void URocketEngine::FullStop(float DeltaTime)
+{
+	if (bFullStop)
+	{
+		auto OwnerVelocity = GetOwner()->GetVelocity();
+		
+		auto Force = OwnerVelocity * ThrustFullStop;
+
+		if (OwnerVelocity.Size() < 300.0f)
+		{
+			Force *= 2.0f;
+		}
+		else if (OwnerVelocity.Size() < 100.0f)
+		{
+			Force *= 8.0f;
+		}
+		else if (OwnerVelocity.Size() < 50.0f)
+		{
+			Force *= 20.0f;
+		}
+		else if (OwnerVelocity.Size() < 25.0f)
+		{
+			Force *= 100.0f;
+		}
+		
+		OwnerRoot->AddForce(-Force);
+	}
+}
+
---
+++ Source/CosmicCastaway/Components/RocketEngine.h
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "RocketEngine.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class COSMICCASTAWAY_API URocketEngine : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	URocketEngine();
+	
+	// Called every frame
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Engine")
+	void StartEngine();
+
+	UFUNCTION(BlueprintCallable, Category = "Engine")
+	void StopEngine();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Engine");
+	float Thrust = 100.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Engine");
+	float ThrustFullStop = 100.0f;
+	
+private:
+	
+	UPrimitiveComponent* OwnerRoot;
+
+	UInputComponent* InputComponent;
+
+	bool bFullStop;
+
+	float Coefficient = 8000.0f;
+
+	void GetOwnerRoot();
+
+	void AddEngineForce(float DeltaTime);
+
+	void BindToInput();
+
+	UFUNCTION()
+	void FullStopTrue();
+
+	UFUNCTION()
+	void FullStopFalse();
+
+	void FullStop(float DeltaTime);
+};
