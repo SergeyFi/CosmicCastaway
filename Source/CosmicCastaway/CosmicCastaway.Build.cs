53b5b44bfcc8aba3e63c789bca5e2bddeacb71fe
Sergey Firsov <46263847+SergeyFi@users.noreply.github.com>
2021-11-03 13:33:40 +0300

    Fix: add Include path - CosmicCastaway

--- Source/CosmicCastaway/CosmicCastaway.Build.cs
+++ Source/CosmicCastaway/CosmicCastaway.Build.cs
@@ -12,6 +12,10 @@ public class CosmicCastaway : ModuleRules
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
+		PublicIncludePaths.AddRange(new string[] {"CosmicCastaway/"});
+
+
+
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
