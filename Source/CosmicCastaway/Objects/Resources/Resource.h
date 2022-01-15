2070f535ee331362e2e2fc4c7f8d0987cc0b3b46
Sergey Firsov <46263847+SergeyFi@users.noreply.github.com>
2022-01-16 00:44:11 +0300

    Feature:add base resource class and oxygen

---
+++ Source/CosmicCastaway/Objects/Resources/Resource.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Resource.generated.h"
+
+/**
+ * 
+ */
+
+USTRUCT(BlueprintType)
+struct FDecompProduct
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<class UResource> Resource;
+
+	UPROPERTY(EditDefaultsOnly)
+	float Weight;
+};
+
+
+UCLASS(Blueprintable, meta = (IsBlueprintBase = "true"))
+class COSMICCASTAWAY_API UResource : public UObject
+{
+	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Resources")
+	FName Name;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Resources")
+	float Weight;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Resources")
+	TArray<FDecompProduct> DecompProducts;
+};
+
